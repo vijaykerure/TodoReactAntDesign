d2000d4ba05878277eb1ebaae344895d2262ce96
vijaykerure <kerurevijay@gmail.com>
2021-10-14 14:24:42 +0530

    Minor fixes

--- src/components/TodoRedux/TodoList.js
+++ src/components/TodoRedux/TodoList.js
@@ -8,27 +8,24 @@ const TodoList = ({ loading, todos = [], onDelete, onToggle, onEditMode, offEdit
       <Skeleton loading={loading} active>
         { todos.length === 0 ? <Empty /> : 
         <List
-          bordered
           dataSource={todos}
           renderItem={({ bordered = false, completed, text, id } = {}) => (
             
-              <List.Item 
-                actions={[
-                  bordered ?<Button type="dashed" onClick={(e) => offEditMode({ id, text: e.currentTarget.previousElementSibling.value })} icon={<SaveOutlined />}> Save </Button> : null, 
-                  <Button type="primary" danger icon={<DeleteOutlined />} onClick={ () => onDelete(id) } />
-                ]}
-              >
-                <Space >
-                  <Checkbox checked={ completed } key={ id } onClick={() => onToggle({ id, completed })} />
-                  
-                  <Input               
-                    onChange={ e => e.target.value = text }
-                    defaultValue={ text }
-                    bordered={ bordered }
-                    onClick={() => onEditMode({ id, text })}                
-                    style={ completed ? { textDecoration: "line-through" } : {}}
-                  />
-                </Space>
+              <List.Item >
+             
+                <Checkbox checked={ completed } key={ id } onClick={() => onToggle({ id, completed })} />
+                
+                <Input               
+                  onChange={ e => e.target.value = text }
+                  defaultValue={ text }
+                  bordered={ bordered }
+                  onClick={() => onEditMode({ id, text })}                
+                  style={ completed ? { textDecoration: "line-through" } : {}}
+                />
+                {
+                  bordered ? <Button type="dashed" onClick={(e) => offEditMode({ id, text: e.currentTarget.previousElementSibling.value })} icon={<SaveOutlined />}> Save </Button> : null
+                }
+                <Button type="primary" danger icon={<DeleteOutlined />} onClick={ () => onDelete(id) } />
               </List.Item>
           )}
         />
